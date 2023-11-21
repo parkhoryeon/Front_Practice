e3c153cb9b8e4faff68eabd95aada44e98e3bc83
parkhoryeon <hrpark930908@naver.com>
2023-11-22 00:46:49 +0900

    Commit
    231122_0046

--- static/scripts/common.js
+++ static/scripts/common.js
@@ -2,14 +2,17 @@ console.log('common.js Connected')
 
 const side_menu = document.querySelector('#header aside')
 const side_close = document.querySelector('.top_top a img')
+const overlay = document.querySelector('#overlay')
 const hamburger = document.querySelector('#header .inner .left li:first-child a img')
 
 hamburger.addEventListener('click', function(event) {
     event.preventDefault();
     side_menu.classList.add('show')
+    overlay.classList.add('show')
 })
 
 side_close.addEventListener('click', function(event) {
     event.preventDefault();
     side_menu.classList.remove('show')
+    overlay.classList.remove('show')
 })
